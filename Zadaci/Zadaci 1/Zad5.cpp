2b4fd12e28a59512aab91b5a9c041bff2e6fcaaa
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 09:45:06 +0200

    Update Zad5.cpp

--- Zadaci/Zadaci 1/Zad5.cpp
+++ Zadaci/Zadaci 1/Zad5.cpp
@@ -1,10 +1,4 @@
-﻿/*
-Omogučiti unos dva broja X i Y iz intervala od 10 do 5000.
-Kreirati program koji će pronaći i ispisati sve brojeve od X do Y (uključujući granične vrijednosti) za koje važi da je obrnuti broj djeljiv sa njegovom sumom cifara. 
-Za olakšanje rješenja zadataka napraviri funkcije za obrtanje poretka cifara i za sumu cifara. 
-Npr. Za broj 144 vrijedi da mu je suma cifara 9 i da je broj koji se dobije nakon što se njegove cifre obrnu 441, djeljiv sa 9.
-Definišite vrijednosti intervala [X, Y]: 11 150 Brojevi koji ispunjuju dati uslov: 18 20 21 24 27
-*/
+
 #include<iostream>
 using namespace std;
 
