87e8f3f2bb518667f267a4d577e14eefa35e6397
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 09:44:28 +0200

    Update Zad4.cpp

--- Zadaci/Zadaci 1/Zad4.cpp
+++ Zadaci/Zadaci 1/Zad4.cpp
@@ -1,7 +1,4 @@
-﻿/*
-Poštujući sve faze procesa programiranja, napisati program koji korisniku omogućava unos cijelog broja N, te izračunava vrijednost sume faktorijela neparnih brojeva u intervalu [1, N]. 
-Suma faktorijela prikazana je sljedećom formulom: S = 1! + 3! + 5! + ... + N!
-*/
+
 #include<iostream>
 using namespace std;
 
