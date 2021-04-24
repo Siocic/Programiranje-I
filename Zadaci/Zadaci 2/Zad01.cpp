2984e725f013c2b0562d545eec42f44307ac3e7a
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:30:39 +0200

    Update and rename Zad1.cpp to Zad01.cpp

--- Zadaci/Zadaci 2/Zad01.cpp
+++ Zadaci/Zadaci 2/Zad01.cpp
@@ -1,11 +1,4 @@
-/*
-Napisati program koji simulira bacanje 3 kockice (jedna kockica ima 6 strana i na tim stranama su brojevi 1 - 6). 
-Simuliranje bacanja svake kockice ostvariti funkcijom rand() % 6 + 1. 
-Simulirati konstantno bacanje sve tri kockice dok se u dva uzastopna bacanja ne desi da se dobiju isti brojevi na sve tri kockice 
-(npr. u šestom bacanju se dobiju brojevi 2, 2, 2, a u sedmom 4, 4, 4 na sve tri kockice).
-Ispisati koliko je ukupno bilo bacanja dok se nije ispunio navedeni uslov.
-Nije potrebno tražiti bilo kakav unos od korisnika.
-*/
+
 #include<iostream>
 using namespace std;
 int baciKocku();
