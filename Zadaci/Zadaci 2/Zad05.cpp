e84d852e7579fe6752cddecfc2247d36f4a361ba
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:31:57 +0200

    Update and rename Zad5.cpp to Zad05.cpp

--- Zadaci/Zadaci 2/Zad05.cpp
+++ Zadaci/Zadaci 2/Zad05.cpp
@@ -1,9 +1,4 @@
-﻿/*
-Napisati program koji omogućava korisniku unos prirodnog broja N (50 < N < 5000000).
-Zatim uneseni broj proslijediti funkciji kao parametar po referenci. 
-U funkciji je od unesenog broja potrebno kreirati novi broj teko što će se poredak cifri unesenog broja obrnuti. Prilikom obrtanja potrebno je odbaciti neparne cifre.
-Npr. Za uneseni broj 1234567 obrnuti broj bez neparnih cifara je 642.
-*/
+
 #include<iostream>
 using namespace std;
 
