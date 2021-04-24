3b4bb6e6216591718a67a68fb48d0b883f3b1043
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 09:43:51 +0200

    Update Zad2.cpp

--- Zadaci/Zadaci 1/Zad2.cpp
+++ Zadaci/Zadaci 1/Zad2.cpp
@@ -1,17 +1,4 @@
-﻿/*
-Napisati program u kojem je potrebno unijeti odabrani broj cifara počevši od cifre najveće težinske vrijednosti, 
-pa od njih sastaviti i napisati prirodni broj. Unos cifara se prekida kada se unese broj manji od 0, ukoliko korisnik unese cifru veću od 9, ispisati poruku da cifra nije validna, ali se unos ne prekida.
 
-(Obavezno koristiti barem jednu korisnički definisanu funkciju)
-
-Ispis neka bude oblika:
-Upisi cifru: 2
-Upisi cifru: 8
-Upisi cifru: 72
-Cifra nije validna
-Upisi cifru: -1
-Broj sastavljen od zadanih cifara je 28.
-*/
 #include<iostream>
 using namespace std;
 int sastavi(int, int&);
