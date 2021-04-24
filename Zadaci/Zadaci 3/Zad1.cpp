e1d76646a2eac9258f37111f9c7d9ff1e1eadc42
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:43:04 +0200

    Update Zad1.cpp

--- Zadaci/Zadaci 3/Zad1.cpp
+++ Zadaci/Zadaci 3/Zad1.cpp
@@ -1,9 +1,4 @@
-﻿/*
-Napisati program koji će omogućiti korisniku da unese veličinu n jednodimenzionalnog dinamičkog niza integer vrijednosti (n > 2).
-Napisati rekurzivnu funkciju koja će taj niz puniti serijom brojeva tako da je svaki sljedeći broj jednak umnošku dva prethodna broja, a prva dva elementa su 1 i 3 (1, 3, 3, 9, 27, ...).
-Zatim napraviti drugu rekurzivnu funkciju koja će izračunati zbir svih elemenata tog niza (voditi računa o tipu podatka zbog veličine broja).
-(Obavezno koristiti aritmetiku pokazivača)
-*/
+
 #include<iostream>
 using namespace std;
 
