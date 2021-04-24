3f0225710b56476a049a08787bb587e39ecbece6
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:43:23 +0200

    Update Zad2.cpp

--- Zadaci/Zadaci 3/Zad2.cpp
+++ Zadaci/Zadaci 3/Zad2.cpp
@@ -1,11 +1,4 @@
-﻿/*
-Napisati program koji će omogućiti korisniku da unese veličinu jednodimenzionalnog dinamičkog niza integer vrijednosti. 
-Napisati rekurzivnu funkciju koja će taj niz popuniti serijom brojeva 2, 4, 8, 16, 32, 64, 128, 256, 512, itd., 
-tako da je svaki sljedeći broj jednak prethodnom pomnoženom sa 2, počevši od broja 2. 
-Zatim,korisnik unosi željeni broj te rekurzivnom funkcijom provjerava da li je taj broj član kreiranog niza. 
-Rekurzija se prekida kada se popuni cijeli niz ili kada se desi overflow na integer varijabli.
-(U ovom zadatku je zabranjeno indeksirati niz uglastim zagradama. Obavezno koristiti aritmetiku pokazivača.)
-*/
+
 #include<iostream>
 using namespace std;
 void puniNiz(int*, int, int);
