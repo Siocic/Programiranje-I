8e3dc0f1d061a40e9e9091f5a4ecf6a229faf8cf
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 09:44:13 +0200

    Update Zad3.cpp

--- Zadaci/Zadaci 1/Zad3.cpp
+++ Zadaci/Zadaci 1/Zad3.cpp
@@ -1,9 +1,4 @@
-﻿/*
-Napisati program koji će omogućiti unos prirodnog broja A (A > 100000),
-te kreirati i ispisati broj B koji će se sastojati od cifara broja A u obrnutom redoslijedu u kojem je svaka parna uklonjena.
-(npr. ako se unese 45362792 funkcija će vratiti 9735). Ispisati razliku brojeva A i B.
-(Obavezno koristiti bar jednu korisnički definiranu funkciju.)
-*/
+
 #include<iostream>
 using namespace std;
 
