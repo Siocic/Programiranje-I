2e0660359f4dc373f1b467e6082e1d10b07a1463
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:44:35 +0200

    Update Zad5.cpp

--- Zadaci/Zadaci 3/Zad5.cpp
+++ Zadaci/Zadaci 3/Zad5.cpp
@@ -1,10 +1,4 @@
-﻿/*
-Napisati program koji će omogućiti korisniku da unese veličinu jednodimenzionalnog dinamičkog niza integer vrijednosti. 
-Napisati rekurzivnu funkciju koja će taj niz popuniti random brojevima u opsegu od 1 do 100. Za generisanje random brojeva koristiti rand() % 100 + 1. 
-Punjenje niza se prekida kada se popuni cijeli niz ili kada se tri puta uzastopno dobije isti random broj. 
-U ovom zadatku je zabranjeno indeksirati elemente niza uglastim zagradama. Obavezno koristiti aritmetiku pokazivača.
-(Obavezno sve riješiti jednom rekurzivnom funcijom.)
-*/
+
 #include<iostream>
 using namespace std;
 
