2c0864365e2d8da5f5b3fc4cd3887d9b53deca8d
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-04-24 10:32:32 +0200

    Update and rename Zad7.cpp to Zad07.cpp

--- Zadaci/Zadaci 2/Zad07.cpp
+++ Zadaci/Zadaci 2/Zad07.cpp
@@ -1,10 +1,4 @@
-﻿/*
-Napisati program koji će omogućiti korisniku unos broja n (uz uslov 10 <= n <= 1000).
-Zatim simulirati unos n slučajnih vrijednosti. Simuliranje unosa ostvariti funkcijom rand() % 1000 + 1.
-Izračunati statističke podatke u kojem procentu se od n generisanih slučajnih vrijednosti pojavljuje prost broj.
-Npr. Ako je generisano 10 brojeva (n = 10): 5,7,4,13,17,20,25,23,30,45, u ovom uzorku od 10 brojeva postoji 5 prostih brojeva pa je procenat prostih brojeva u ovom slučaju 50%.
-(Napomena: Po želji kreirati dodatne funkcije da se olakša rješavanje zadatka.)
-*/
+
 #include<iostream>
 using namespace std;
 
