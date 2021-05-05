6dab49a86de4205d4dd84c574c79fc28cc2c6c8f
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-05-05 18:04:07 +0200

    Add files via upload

---
+++ Zadaci/Zadaci 2/Zad10.cpp
@@ -0,0 +1,70 @@
+/*
+Napisati program koji ce omoguciti korisniku unos broj n (uz uslov 10<=n<=1000). 
+Zatim simulirati unos n slucajnih vrijednosti. Simuliranje unosa ostvariti funkcijom rand()%1000+1. 
+Izracunati statisticke podatke u kojem procentu se od n generisanih slucajnih vrijednosti pojavljuje savrsen broj. 
+Za neki broj se kaze da je savrsen ukoliko je jednak sumi svih svojih djelilaca (ne ukljucujici njega samog).
+Na primjer, 28 je savrsen broj: njegovi djelioci su 1, 2, 4, 7 i 14, a 1 + 2 + 4 + 7 + 14 = 28. 
+Jedini savrseni brojevi u opsegu od 1 do 100 su brojevi 6 i 28.
+*/
+#include <iostream>
+
+using namespace std;
+
+bool provjeraSavrsen(int);
+void savrsenBroj(int);
+int simulacijaUnosa();
+
+int main() {
+    int n;
+
+    srand(time(0));
+    do
+    {
+        n = simulacijaUnosa();
+
+    } while (n <= 10 && n <= 1000);
+
+    savrsenBroj(n);
+
+    system("pause>0");
+    return 0;
+}
+int simulacijaUnosa()
+{
+    int x;
+    x = rand() % 1000 + 1;
+    return x;
+}
+
+bool provjeraSavrsen(int n) {
+
+    int suma = 0;
+
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            suma += i;
+        }
+    }
+    return suma == n;//ako nam je broj djeljitelja jednak n broj je savrsen
+}
+void savrsenBroj(int n)
+{
+    int brojac = 0;
+    cout << "Savrseni brojevi od 1 do " << n << " su: ";
+
+    for (int i = 1; i <= n; i++) {
+        if (provjeraSavrsen(i)) {
+            cout << i << " ";
+            brojac++;
+        }
+    }
+    cout << endl;
+	
+    float procenat = 0;
+   // float rezultat = float(brojac) / float(n);//kao int dobije 0.2 uzimat cemo 0*100 i dobijemo 0%, a kao float 0.2*100% dobije se 2%
+    procenat = (100 * float(brojac)) / float(n);
+    cout << "Procenat prostih brojeva je: " << procenat << " %. " << endl;
+    
+}
