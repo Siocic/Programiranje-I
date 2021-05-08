6e0482db6a3564ed278fc395504eee5d6e93c842
Faris Siocic <77838860+Siocic@users.noreply.github.com>
2021-05-08 23:59:36 +0200

    Update Zad1.cpp

--- Zadaci/Zadaci 4/Zad1.cpp
+++ Zadaci/Zadaci 4/Zad1.cpp
@@ -1,19 +1,4 @@
-﻿/*
- Kreirati dinamički dvodimenzionalni niz čiji su elementi objekti tipa fudbaler(fudbaler je struktura čija su obilježja data u nastavku).Omogućiti korisniku da unese dimenzije niza.
- struct fudbaler
-{
-    int* godRodjenja;
-    int* brojIgraca;
-    int* golovi;
-};
-Redovi dvodimenzionalnog niza predstavljaju različite timove, a kolone predstavljaju igrače u tim timovima.
-Omogućiti korisniku unos svih podataka za sve igrače, te napraviti(i testirati u main funkciji) funkciju koja će pronaći tim sa najvećim prosjekom.
-Zatim napraviti(i testirati u main funkciji) funkciju koja će pronaći igrača sa najviše golova u timu sa najlošijim prosjekom golova.
-Radi lakšeg testiranja programa obavezno u funkciji unosa podataka omogućiti da se podaci generišu kao sljedeće :
-godRodjenja: 1980 + rand() % ((2000 + 1) - 1980) //
-Generiše brojeve u rangu 1980 - 2000 brojIgraca : rand() % 30 + 1 golovi : rand() % 5
-(U ovom zadatku je zabranjeno indexirati elemente niza uglastim zagradama.Obavezno koristiti aritmetiku pokazivača.)
- */
+
 #include <iostream>
 using namespace  std;
 
